f4739eb11f0e58e221c0a0c25e26d8ad251d5741
oat431 <oat431@gmail.com>
2023-10-03 03:09:52 +0700

    🛠 setup: authorization server

---
+++ src/test/java/panomete/playground/oauthserver/OauthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.playground.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
